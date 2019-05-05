915f5fdfcc2d58f846389bb55140c961bf4d8a61
directdevops <khaja@directdevops.blog>
2019-05-05 13:49:29 +0530

    Add project files.

---
+++ JenkinsBuildExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsBuildExample.Startup))]
+namespace JenkinsBuildExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
